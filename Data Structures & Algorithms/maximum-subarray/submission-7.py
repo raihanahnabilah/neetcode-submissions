7e326149c0f6c0474620ee447c693bae3aa1d89d
Hana Nabilah Fatinah <56430915+raihanahnabilah@users.noreply.github.com>
2026-05-18 20:29:08 +0800

    Add: maximum-subarray - submission-7

---
+++ Data Structures & Algorithms/maximum-subarray/submission-7.py
@@ -0,0 +1,20 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        # 2+-3 = -1. bad?
+        # -3+4 = 1. quite good 
+        # -3+4+-2 = -1. bad. lets decrement previous one
+        # 4+-2 = 2 good.
+        # 4+-2+2 = 4. good
+
+        max_val = float("-inf")
+        curr_val = 0
+        for num in nums:
+            curr_val += num
+            max_val = max(max_val, curr_val)
+
+            if curr_val < 0:
+                curr_val = 0
+        
+        return max_val
+
+
