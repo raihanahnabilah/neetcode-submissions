d2258c2b2de36f0c0e04bf7cbaa8eb832a571d6f
Hana Nabilah Fatinah <56430915+raihanahnabilah@users.noreply.github.com>
2026-05-19 17:34:16 +0800

    Add: anagram-groups - submission-7

---
+++ Data Structures & Algorithms/anagram-groups/submission-7.py
@@ -0,0 +1,32 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        
+        # method 1:
+        # a bit brute force:
+        # sort each word, and put it in a map
+        # time complexity: O(n^2logn)
+        # space complexity: O(n)
+
+        # map_set_to_strings = {}
+        # for word in strs: # O(n)
+        #     sorted_word = "".join(sorted(word)) # O(nlogn)
+        #     if sorted_word in map_set_to_strings:
+        #         map_set_to_strings[sorted_word].append(word)
+        #     else:
+        #         map_set_to_strings[sorted_word] = [word]
+        
+        # return list(map_set_to_strings.values())
+
+        # solution 2
+        map_charcount_to_string = {}
+
+        for word in strs: # O(n^2)
+            char_count = [0] * 26
+            for char in word:
+                char_count[ord(char) - ord('a')] += 1
+            if tuple(char_count) in map_charcount_to_string:
+                map_charcount_to_string[tuple(char_count)].append(word)
+            else:
+                map_charcount_to_string[tuple(char_count)] = [word]
+
+        return list(map_charcount_to_string.values())
