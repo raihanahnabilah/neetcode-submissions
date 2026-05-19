e4201a58edd1186c694912fda6e4fd51063898bf
Hana Nabilah Fatinah <56430915+raihanahnabilah@users.noreply.github.com>
2026-05-19 18:25:15 +0800

    Add: jump-game - submission-11

---
+++ Data Structures & Algorithms/jump-game/submission-11.py
@@ -0,0 +1,41 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        # 1,2,0,1,0
+
+        # target: index 4
+        # logic for bottom up, at any point, check if it's 4
+        #   0
+        #.      +1
+        #.       1
+        #.    +1  +2
+        #     2.   3
+        #    stuck +1
+        #           4 
+        # 4 is there, so we're good!
+
+        # logic for top down
+        #.        4
+        #       index 4 = we dont care about the value
+        #       index 3 + 1 =4 -> can reach 4, we're good
+        #       index 2 + 0 = 2 -> cant reach 3, we're not good
+        #       index 1 + 2 = 3 -> can reach 3, we're good
+        #       index 0 + 1 = 1 -> can reach 1, we're good
+        
+        # time complexity: O(n)
+        # space complexity: O(1)
+        target = len(nums)-1
+        for i in range(len(nums)-1, -1, -1):
+            if i == len(nums)-1:
+                continue
+            if i + nums[i] >= target:
+                target = i
+        
+        if target == 0:
+            return True
+        
+        return False
+
+
+            
+                
+
